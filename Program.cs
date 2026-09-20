643a12a2fbba0f36ab3a8b0b479780358804f0c0
EiPyaeKyaw <eipyae.23@ichat.sp.edu.sg>
2026-09-20 12:44:47 +0800

    Connet to SQL server and print all device rows

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+using Microsoft.Data.SqlClient;
+
+const string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=ICAS_Test;Trusted_Connection=True;";
+
+using var connection = new SqlConnection(ConnectionString);
+connection.Open();
+
+using var command = new SqlCommand("SELECT * FROM dbo.t_DeviceCfg", connection);
+using var reader = command.ExecuteReader();
+
+PrintHeader(reader);
+while (reader.Read())
+{
+    PrintRow(reader);
+}
+
+static void PrintHeader(SqlDataReader reader)
+{
+    var headers = new string[reader.FieldCount];
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+        headers[i] = reader.GetName(i);
+    }
+    Console.WriteLine(string.Join(" | ", headers));
+}
+
+static void PrintRow(SqlDataReader reader)
+{
+    var values = new string[reader.FieldCount];
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+        values[i] = reader.IsDBNull(i) ? "NULL" : reader.GetValue(i).ToString() ?? "";
+    }
+    Console.WriteLine(string.Join(" | ", values));
+}
